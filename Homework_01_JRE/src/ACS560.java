9afffc335822bcd86cf36d79d41cb4b7f315f36b
mahap01 <mahap01@pfw.edu>
2023-09-02 23:47:46 -0400

    Homework_01_error

---
+++ Homework_01_JRE/src/ACS560.java
@@ -0,0 +1,11 @@
+/* 
+ Location: Purdue University Fort Wayne
+ Academia: Fall 2023
+ Project: Homework_1
+ Guidance: Prof.Matthew Parker
+ */
+public class ACS560 {
+	public static void main(String[] Args) {
+		System.out.println("Hello ACS560!");
+	}
+}
---
+++ Homework_01_Py/main.py
@@ -0,0 +1,14 @@
+# Location: Purdue University Fort Wayne
+# Academia: Fall 2023
+# Project: Homework_1
+# Guidance: Prof.Matthew Parker
+class ACS560:
+
+    # Simple print statement
+    statement = "Hello ACS560!"
+
+# variable = class.attribute
+attribute = ACS560.statement
+
+# Print the variable value
+print(attribute)
