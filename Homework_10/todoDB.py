7b7ce2ba9b4a5f23c5cd1cea35f9f0eab0ba3b5e
mahap01 <mahap01@pfw.edu>
2023-12-04 23:37:43 -0500

    HW10 resubmission

--- Homework_10/todoDB.py
+++ Homework_10/todoDB.py
@@ -1,69 +1,122 @@
+"""
+University: Purdue University Fort Wayne
+Semester: Fall 2023
+Class: ACS560
+Professor: Matthew Parker
+Assignment: Homework_12
+Goal: API to interact with sqlite DB
+Submited by: Prasad Sadanand Mahabare
+"""
+# Import necessary modules
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
+# Initialize Flask app
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Todo.db'
+
+# Configure database connection
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///restaurant.db'  # SQLite database URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
-    __tablename__ = 'todos'
+# Define a database model for restaurant menus
+class RestaurantMenu(db.Model):
+    __tablename__ = 'restaurant_menus'
 
     id = db.Column(db.Integer, primary_key=True)
-    task = db.Column(db.String(255), nullable=False)
-    start_date = db.Column(db.Date)
-    finish_date = db.Column(db.Date)
+    dish_name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    price = db.Column(db.Float, nullable=False)
+    vegetarian = db.Column(db.Boolean, default=False)
+    available = db.Column(db.Boolean, default=True)
 
-@app.route('/todos', methods=['GET'])
-def get_todos():
-    todos = Todo.query.all()
-    todos_data = []
-    for todo in todos:
-        todo_data = {
-            "id": todo.id,
-            "task": todo.task,
-            "start_date": str(todo.start_date) if todo.start_date else None,
-            "finish_date": str(todo.finish_date) if todo.finish_date else None
-        }
-        todos_data.append(todo_data)
-    return jsonify(todos_data)
+# Create tables
+with app.app_context():
+    db.create_all()
 
-@app.route('/todos', methods=['POST'])
-def add_todo():
+# Route to add a menu item
+@app.route('/Menus', methods=['POST'])
+def add_menu():
     data = request.json
-    todo = Todo(task=data['task'], start_date=data['start_date'], finish_date=data['finish_date'])
-    db.session.add(todo)
+    menu = RestaurantMenu(
+        dish_name=data['dish_name'],
+        description=data.get('description', ''),
+        price=data['price'],
+        vegetarian=data['vegetarian'],
+        available=data.get('available', True)
+    )
+    db.session.add(menu)
     db.session.commit()
-    return "Todo added successfully", 201
+    return "Menu item added successfully", 201
 
-@app.route('/todos/<int:id>', methods=['PUT'])
-def update_todo(id):
-    todo = Todo.query.get(id)
-    if todo is None:
-        return jsonify({"message": "Todo not found"}), 404
+# Route to fetch all restaurant menus
+@app.route('/Menus', methods=['GET'])
+def get_menus():
+    menus = RestaurantMenu.query.all()
+    menus_data = [{
+        "id": menu.id,
+        "dish_name": menu.dish_name,
+        "description": menu.description,
+        "price": menu.price,
+        "vegetarian": menu.vegetarian,
+        "available": menu.available
+    } for menu in menus]
+    return jsonify(menus_data)
+
+# Route to fetch a specific menu by ID
+@app.route('/Menus/<int:id>', methods=['GET'])
+def get_menu(id):
+    menu = RestaurantMenu.query.get(id)
+    
+    if menu is None:
+        return jsonify({"message": "Menu item not found"}), 404
+    
+    menu_data = {
+        "id": menu.id,
+        "dish_name": menu.dish_name,
+        "description": menu.description,
+        "price": menu.price,
+        "vegetarian": menu.vegetarian,
+        "available": menu.available
+    }
+    
+    return jsonify(menu_data)
+
+# Route to update a menu item by ID
+@app.route('/Menus/<int:id>', methods=['PUT'])
+def update_menu(id):
+    menu = RestaurantMenu.query.get(id)
+    
+    if menu is None:
+        return jsonify({"message": "Menu item not found"}), 404
+
     try:
-        data = request.get_json()
-        if 'start_date' in data:
-            todo.start_date = data['start_date']
-        if 'finish_date' in data:
-            todo.finish_date = data['finish_date']
+        data = request.json
+        # Update fields based on the data received
+        if 'description' in data:
+            menu.description = data['description']
+        if 'price' in data:
+            menu.price = data['price']
+        if 'vegetarian' in data:
+            menu.vegetarian = data['vegetarian']
+        if 'available' in data:
+            menu.available = data['available']
+
         db.session.commit()
-        return jsonify({"message": "Todo updated successfully"})
+        return jsonify({"message": "Menu item updated successfully"})
     except Exception as e:
         return jsonify({"message": str(e)}), 500
 
-@app.route('/todos/<int:id>', methods=['DELETE'])
-def delete_todo(id):
-    todo = Todo.query.get(id)
-    if todo:
-        db.session.delete(todo)
+# Route to delete a menu item by ID
+@app.route('/Menus/<int:id>', methods=['DELETE'])
+def delete_menu(id):
+    menu = RestaurantMenu.query.get(id)
+    if menu:
+        db.session.delete(menu)
         db.session.commit()
-        return "Todo deleted successfully", 200
+        return jsonify({"message": "Menu item deleted successfully"}), 200
     else:
-        return "Todo not found", 404
-
-# Other routes and functionalities can be added as per requirements
+        return jsonify({"message": "Menu item not found"}), 404
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
